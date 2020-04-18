4643730e6757cb12596fe3b9216b8a2240f1caeb
Sarath T <mailtsarath@yahoo.com>
2020-04-17 23:33:41 -0500

    Updates

---
+++ precache-manifest.d0d92e6e6c4a2f2f7ec2f865bbd37d0f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b1c2809d4730d1070c4ebb00ef32fd3",
+    "url": "/COVID-19--React-JS/index.html"
+  },
+  {
+    "revision": "a5a1c688384140b05fdb",
+    "url": "/COVID-19--React-JS/static/css/main.a1e48035.chunk.css"
+  },
+  {
+    "revision": "eea3d912086d605a3239",
+    "url": "/COVID-19--React-JS/static/js/2.ab7e95c0.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/COVID-19--React-JS/static/js/2.ab7e95c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a5a1c688384140b05fdb",
+    "url": "/COVID-19--React-JS/static/js/main.afa53889.chunk.js"
+  },
+  {
+    "revision": "7a468bf1beb45768c51b",
+    "url": "/COVID-19--React-JS/static/js/runtime-main.84196a0d.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/COVID-19--React-JS/static/media/COVID.d7265326.png"
+  }
+]);
